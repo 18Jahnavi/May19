0d9bec3094d1adebe7940789e545a7b747626577
Jahnavi <vjahnavi460@gmail.com>
2026-07-13 10:24:35 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+I am in Main Branch
+I am in dev
+I am in feature 2
---
+++ script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    alert("Assignment: Create feature3 and feature4 branches, resolve conflicts, and merge into main.");
+}
